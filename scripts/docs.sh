653d59a6a3d818e172ca0285ab4f08c7136c17be
Think Wu <yingxinwu.g@gmail.com>
2015-08-02 07:55:36 +0800

    + GA tracking

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -11,3 +11,29 @@ mv docs $SITE_DIR/
 
 cd $SITE_DIR
 git checkout -- docs/docco.css  # drop the auto-gen diff
+
+# insert google analytics tracker
+OS=`uname`
+case $OS in
+  Darwin )
+    SED_CMD="sed -i .bak"
+    ;;
+  * )
+    SED_CMD="sed -i"
+    ;;
+esac
+
+read -d '' GA_SCRIPT <<"EOF"
+<script type="text/javascript">\
+  var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");\
+  document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));\
+</script>\
+<script type="text/javascript">\
+  try {\
+    var pageTracker = _gat._getTracker("UA-33049696-1");\
+  pageTracker._trackPageview();\
+  } catch(err) {}\
+</script>
+EOF
+# echo $GA_SCRIPT
+find docs -type f -name "*.html" -exec $SED_CMD "s|</body>|${GA_SCRIPT}</body>|g" '{}' \;
