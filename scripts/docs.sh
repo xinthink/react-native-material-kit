c41a89e907553cb4615dd25f8cc7c8e6405a6ba0
Think Wu <yingxinwu.g@gmail.com>
2015-08-02 15:22:02 +0800

    add sitemap

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/zsh
 SITE_DIR=$(cd `dirname $0`/.. && pwd)
 LIB_DIR=$(cd `dirname $0`/../../rct-material-kit/Source && pwd)
 
 cd $LIB_DIR
-docco-central 'Lib/**/*.js'
+docco-central --title 'Index' \
+  --windowTitle 'Annotated Source - RN Material Kit' \
+  'Lib/**/*.js'
 gulp docs
 
 rm -rf $SITE_DIR/docs
@@ -13,28 +15,14 @@ cd $SITE_DIR
 git checkout -- docs/docco.css  # drop the auto-gen diff
 
 # insert google analytics tracker
-OS=`uname`
-case $OS in
-  Darwin )
-    SED_CMD="sed -i .bak"
-    ;;
-  * )
-    SED_CMD="sed -i"
-    ;;
-esac
-
 read -d '' GA_SCRIPT <<"EOF"
-<script type="text/javascript">\
-  var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");\
-  document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));\
-</script>\
-<script type="text/javascript">\
-  try {\
-    var pageTracker = _gat._getTracker("UA-33049696-1");\
-  pageTracker._trackPageview();\
-  } catch(err) {}\
-</script>
+<script type="text/javascript"> var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www."); document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E")); </script> <script type="text/javascript"> try {var pageTracker = _gat._getTracker("UA-33049696-1"); pageTracker._trackPageview(); } catch(err) {} </script>
 EOF
 # echo $GA_SCRIPT
-find docs -type f -name "*.html" -exec $SED_CMD "s|</body>|${GA_SCRIPT}</body>|g" '{}' \;
-find docs -type f -name "*.html" -exec $SED_CMD -E "s/(FIXME|TODO)/<strong><i>\1<\/i><\/strong>/g" '{}' \;
+for f in docs/**/*.html; do
+  sed -i.bak "s|</body>|${GA_SCRIPT}</body>|g" $f
+  sed -i.bak -E "s/(FIXME|TODO)/<strong><i>\1<\/i><\/strong>/g" $f
+done
+
+# sitemap
+ls -l docs/**/*.html | egrep -o 'docs.*' | sed 's|docs|http://www.xinthink.com/react-native-material-kit/docs|g' > sitemap-docs.txt
