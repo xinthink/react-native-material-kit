c998758e1d109f123d08856351f31f140a7ddc67
Think Wu <yingxinwu.g@gmail.com>
2015-08-12 23:05:33 +0800

    update Textfield source
    use new GA tracker

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -16,11 +16,11 @@ git checkout -- docs/docco.css  # drop the auto-gen diff
 
 # insert google analytics tracker
 read -d '' GA_SCRIPT <<"EOF"
-<script type="text/javascript"> var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www."); document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E")); </script> <script type="text/javascript"> try {var pageTracker = _gat._getTracker("UA-33049696-1"); pageTracker._trackPageview(); } catch(err) {} </script>
+<script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-33049696-2', 'auto'); ga('send', 'pageview'); </script>
 EOF
 # echo $GA_SCRIPT
 for f in docs/**/*.html; do
-  sed -i.bak "s|</body>|${GA_SCRIPT}</body>|g" $f
+  sed -i.bak "s^</body>^${GA_SCRIPT}</body>^g" $f
   sed -i.bak -E "s/(FIXME|TODO)/<strong><i>\1<\/i><\/strong>/g" $f
 done
 
