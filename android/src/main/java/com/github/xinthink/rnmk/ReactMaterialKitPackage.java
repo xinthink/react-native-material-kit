a67a52481d0352a4852691395327dc609dbc4f5d
Pete <brunsy@users.noreply.github.com>
2017-08-02 13:21:07 +1200

    handle RN 0.47 breaking change

--- android/src/main/java/com/github/xinthink/rnmk/ReactMaterialKitPackage.java
+++ android/src/main/java/com/github/xinthink/rnmk/ReactMaterialKitPackage.java
@@ -22,7 +22,7 @@ public class ReactMaterialKitPackage implements ReactPackage {
         return Collections.emptyList();
     }
 
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
