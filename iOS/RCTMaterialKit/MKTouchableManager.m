1f76d2bb6b0f8bcfea701cd3b336d6cdf97d48fd
Kelsey Regan <kelsey.regan@lightspeedhq.com>
2019-07-30 11:30:55 -0700

    Fixes react 0.60 issue with deprecated sendInutEventWithName:

--- iOS/RCTMaterialKit/MKTouchableManager.m
+++ iOS/RCTMaterialKit/MKTouchableManager.m
@@ -56,7 +56,7 @@ RCT_EXPORT_MODULE()
                            @"x": [NSNumber numberWithFloat:location.x],
                            @"y": [NSNumber numberWithFloat:location.y],
                            };
-    [self.bridge.eventDispatcher sendInputEventWithName:@"topChange" body:dict];
+    [self.bridge enqueueJSCall:@"RCTEventEmitter" method:@"receiveEvent" args:@[dict[@"target"], RCTNormalizeInputEventName(@"topChange"), dict] completion:NULL];
 }
 
 @end
