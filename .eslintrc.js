8cd10a0de4ab4187e69b99211122e02cf98ad306
Think Wu <yingxinwu.g@gmail.com>
2020-01-07 22:48:30 +0800

    update project settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,9 @@ module.exports = {
     'prettier/prettier': ['error', {
       singleQuote: true,
       trailingComma: 'es5',
-      printWidth: 100,
+      printWidth: 110,
     }],
     semi: 'off',
+    'react-native/no-inline-styles': 'off',
   },
 };
